87fe8bce5c073c053d6060b7abb38a854647b761
Baptiste COENON <bcoenon@e2r6p16.clusters.42paris.fr>
2022-01-31 18:06:56 +0100

    add 4/7 operations fonctions

--- srcs/operations.c
+++ srcs/operations.c
@@ -6,54 +6,77 @@
 /*   By: bcoenon <bcoenon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 16:29:52 by bcoenon           #+#    #+#             */
-/*   Updated: 2022/01/31 17:21:09 by bcoenon          ###   ########.fr       */
+/*   Updated: 2022/01/31 18:00:42 by bcoenon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_stack	sa_sb(t_stack st)
+void	sa_sb(t_stack *st)
 {
 	int	temp;
 
-	temp = st.address[0];
-	st.address[0] = st.address[1];
-	st.address[1] = st.address[temp];
-	if (st.name == 'a')
+	temp = st->address[0];
+	st->address[0] = st->address[1];
+	st->address[1] = st->address[temp];
+	if (st->name == 'a')
 		write(1, "sa\n", 3);
-	else if (st.name == 'b')
+	else if (st->name == 'b')
 		write(1, "sb\n", 3);
-	return (st);
 }
 
-t_stack	ss(t_stack st)
+void	ss(t_stack *st, t_stack *st2)
 {
 	int	temp;
 
-	temp = st.address[0];
-	st.address[0] = st.address[1];
-	st.address[1] = st.address[temp];
-	if (st.name == 'b')
-		write(1, "ss\n", 3);
-	return (st);
+	temp = st->address[0];
+	st->address[0] = st->address[1];
+	st->address[1] = st->address[temp];
+	temp = st2->address[0];
+	st2->address[0] = st2->address[1];
+	st2->address[1] = st2->address[temp];
+	write(1, "ss\n", 3);
 }
 
-t_stack	ra_rb(t_stack st)
+void	ra_rb(t_stack *st)
 {
 	int	i;
 	int	temp;
 
 	i = 0;
-	temp = st.address[i];
-	while (i < st.len)
+	temp = st->address[i];
+	while (i < st->len)
 	{
-		st.address[i] = st.address[i + 1];
+		st->address[i] = st->address[i + 1];
 		i++;
 	}
-	st.address[0] = temp;
-	if (st.name == 'a')
+	st->address[0] = temp;
+	if (st->name == 'a')
 		write(1, "ra\n", 3);
-	else if (st.name == 'b')
+	else if (st->name == 'b')
 		write(1, "rb\n", 3);
-	return (st);
+}
+
+void	rr(t_stack *st, t_stack *st2)
+{
+	int	i;
+	int	temp;
+
+	i = 0;
+	temp = st->address[i];
+	while (i < st->len)
+	{
+		st->address[i] = st->address[i + 1];
+		i++;
+	}
+	st->address[0] = temp;
+	i = 0;
+	temp = st2->address[i];
+	while (i < st2->len)
+	{
+		st2->address[i] = st2->address[i + 1];
+		i++;
+	}
+	st2->address[0] = temp;
+	write(1, "rr", 3);
 }
