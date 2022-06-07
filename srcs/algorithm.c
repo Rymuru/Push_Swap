36ff2d6a60e1242646b4e52b2111f0bb490fd044
bcoenon <bcoenon@student.42.fr>
2022-06-08 01:55:06 +0200

    rm test debut algo

--- srcs/algorithm.c
+++ srcs/algorithm.c
@@ -6,7 +6,7 @@
 /*   By: bcoenon <bcoenon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 16:02:30 by bcoenon           #+#    #+#             */
-/*   Updated: 2022/04/06 14:21:39 by bcoenon          ###   ########.fr       */
+/*   Updated: 2022/06/08 01:53:59 by bcoenon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,20 @@
 int	solver(t_stack *a)
 {
 	t_stack	*b;
+	int		i;
 
 	b = malloc(sizeof(t_stack));
+	i = 0;
 	if (is_solved(a) == 0)
 		return (0);
 	stack_init(b, a->len, 'b');
 	if (b->len == 0)
 		error_manager(a);
+	while (i < a->len)
+	{
+		bitwise(a->address[i], a, b);
+		i++;
+	}
 	return (0);
 }
 
